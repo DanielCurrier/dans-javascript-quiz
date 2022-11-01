84cf0717e92441668095bdf1d43bb5b144621187
Dan Currier <dcurrier585@gmail.com>
2022-11-01 15:20:51 -0400

    added end page with forms

--- Assets/game.js
+++ Assets/game.js
@@ -111,6 +111,7 @@ function startGame() {
 
 function getNewQuestion() {
     if (availableQuestions.length === 0 || questionCounter >= maxQuestions) {
+        localStorage.setItem('mostRecentScore', score);
         return window.location.assign('end.html');
     }
     questionCounter++;
